e065bec65f74d72652c52bdea5d7f0abf55b067d
Frozensun47 <sagar.srivastava.cd.mec20@itbhu.ac.in>
2023-03-11 20:39:06 +0530

    added skip connections, increased number of layers, changes layer image saving folder'

--- autoencoder.py
+++ autoencoder.py
@@ -1,4 +1,4 @@
-from tensorflow.keras import regularizers
+from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D, Input, UpSampling2D,Add
 from tensorflow.keras.losses import MeanSquaredError
@@ -20,9 +20,10 @@ def create_dir(dir_name):
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
-
-# Create a directory to save the output images of each layer
-create_dir('outputimgs')
+# Create a directory to save the output images
+output_dir = 'outputimgs'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 
 # Define the directories containing the input images
@@ -30,9 +31,9 @@ input_dir = 'Images'
 
 # Create data generators to load and preprocess the images
 datagen = ImageDataGenerator(
-    preprocessing_function=lambda x: np.clip(x + np.random.normal(0, 50, x.shape),0, 255).astype(np.uint8)
+    # preprocessing_function=lambda x: np.clip(x + np.random.normal(0, 10, x.shape),0, 255).astype(np.uint8)
 )
-batch_size = 8
+batch_size = 4
 generator = datagen.flow_from_directory(
     input_dir,
     target_size=(256, 256),
@@ -47,68 +48,102 @@ steps_per_epoch = generator.n // batch_size
 # input_shape = generator.image_shape
 # print(num_classes,num_samples,input_shape)
 
-# Define the original model with skip connections
 
 # Define the original model with skip connections
-input_layer = Input(shape=(256, 256, 3))
-conv1 = Conv2D(64, kernel_size=(3, 3), strides=(2, 2),
-               activation=activation_type, padding=padding_type,
-               kernel_regularizer=regularizers.l2(0.01))(input_layer)
-skip1 = conv1
+input_img = Input(shape=(256, 256, 3))
 
-conv2 = Conv2D(64, kernel_size=(3, 3), strides=(2, 2),
-               activation=activation_type, padding=padding_type,
-               kernel_regularizer=regularizers.l2(0.01))(conv1)
-skip2 = conv2
+# # Encoder
+# conv = Conv2D(64, kernel_size=(3, 3), activation='relu',
+#                padding='same')(input_img)
+# pool = MaxPooling2D(pool_size=(2, 2))(conv)
+# conv1 = Conv2D(64, kernel_size=(3, 3),
+#                activation='relu', padding='same')(pool) #128
+# pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+# conv2 = Conv2D(64, kernel_size=(3, 3),
+#                activation='relu', padding='same')(pool1) #256
+# pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+# conv3 = Conv2D(64, kernel_size=(3, 3),
+#                activation='relu', padding='same')(pool2)#512
+# pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+# conv4 = Conv2D(64, kernel_size=(3, 3),
+#                activation='relu', padding='same')(pool3)#1024
 
-conv3 = Conv2D(64, kernel_size=(3, 3), strides=(2, 2),
-               activation=activation_type, padding=padding_type,
-               kernel_regularizer=regularizers.l2(0.01))(conv2)
-conv4 = Conv2D(64, kernel_size=(3, 3), strides=(2, 2),
-               activation=activation_type, padding=padding_type,
-               kernel_regularizer=regularizers.l2(0.01))(conv3)
+# Encoder
+conv = Conv2D(8, kernel_size=(3, 3), activation='relu',
+              padding='same')(input_img)
+conv1 = Conv2D(16, kernel_size=(3,3),
+               strides=(2, 2), activation='relu', padding='same')(conv)  # 128
+conv2 = Conv2D(32, kernel_size=(3, 3),
+               strides=(2, 2), activation='relu', padding='same')(conv1)  # 256
+conv3 = Conv2D(64, kernel_size=(3, 3),
+               strides=(2, 2), activation='relu', padding='same')(conv2)  # 512
+conv4 = Conv2D(64, kernel_size=(3, 3),
+               strides=(2, 2), activation='relu', padding='same')(conv3)  # 1024
 
 
-upsamp1 = UpSampling2D(size=(2, 2))(conv4)
-conv5 = Conv2D(64, kernel_size=(3, 3), activation=activation_type,
-               padding=padding_type, kernel_regularizer=regularizers.l2(0.01))(upsamp1)
+# Decoder
+up = UpSampling2D(size=(2, 2))(conv4)
+conv5 = Conv2D(64, kernel_size=(3, 3), activation='relu',
+               padding='same')(up)  # 512
+add1 = Add()([conv3, conv5])
+conv6 = Conv2D(64, kernel_size=(3, 3),
+               activation='relu', padding='same')(add1)  # 512
 
-upsamp2 = UpSampling2D(size=(2, 2))(conv5)
-conv6 = Conv2D(64, kernel_size=(3, 3), activation=activation_type,
-               padding=padding_type, kernel_regularizer=regularizers.l2(0.01))(upsamp2)
+up1 = UpSampling2D(size=(2, 2))(conv6)
+conv7 = Conv2D(32, kernel_size=(3, 3), activation='relu',
+               padding='same')(up1)  # 256
+add2 = Add()([conv2, conv7])
+conv8 = Conv2D(32, kernel_size=(3, 3),
+               activation='relu', padding='same')(add2)  # 256
 
-add1 = Add()([conv6, skip2])
+up2 = UpSampling2D(size=(2, 2))(conv8)
+conv9 = Conv2D(16, kernel_size=(3,3), activation='relu',
+               padding='same')(up2)  # 128
+add3 = Add()([conv1, conv9])
+conv10 = Conv2D(16, kernel_size=(3, 3),
+                activation='relu', padding='same')(add3)  # 128
 
-upsamp3 = UpSampling2D(size=(2, 2))(add1)
-conv7 = Conv2D(64, kernel_size=(3, 3), activation=activation_type,
-               padding=padding_type, kernel_regularizer=regularizers.l2(0.01))(upsamp3)
+up3 = UpSampling2D(size=(2, 2))(conv10)
+conv11 = Conv2D(8, kernel_size=(3, 3), activation='relu',
+                padding='same')(up3)  # 64
+add4 = Add()([conv, conv11])
+conv12 = Conv2D(8, kernel_size=(3, 3),
+                activation='relu', padding='same')(add4)  # 64
 
-add2 = Add()([conv7, skip1])
+output_img = Conv2D(3, kernel_size=(
+    1, 1), activation='relu', padding='same')(conv12)
 
-upsamp4 = UpSampling2D(size=(2, 2))(add2)
-conv8 = Conv2D(3, kernel_size=(3, 3), activation=activation_type,
-               padding=padding_type, kernel_regularizer=regularizers.l2(0.01))(upsamp4)
+model = Model(inputs=input_img, outputs=output_img)
 
-model = Model(inputs=input_layer, outputs=conv8)
+
 
 plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)
 
 #resume trainng
-# model.load_weights('my_model.h5')
+model.load_weights('my_model.h5')
 
 # Load an image
-img = cv2.imread('noise_img.png')
+img = cv2.imread('inp.png')
 img = cv2.resize(img, (256, 256))
 x = np.expand_dims(img, axis=0)
 
 # Set the loss function to be mean squared error (MSE)
 loss_function = MeanSquaredError()
 
+checkpoint_filepath = 'weights/weights.h5'
+model_checkpoint_callback = ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=True,
+    monitor='loss',
+    mode='min',
+    save_best_only=True)
+
 # Compile the model with the MSE loss
 model.compile(optimizer='adam', loss=loss_function)
 
 # Train the model using the training data
-history = model.fit(generator, epochs=10, steps_per_epoch=steps_per_epoch , verbose=1)
+history = model.fit(generator, epochs=10, steps_per_epoch=steps_per_epoch,
+                    verbose=1, callbacks=[model_checkpoint_callback])
 
 # Plot the training loss over the epochs
 plt.plot(history.history['loss'])
@@ -120,20 +155,26 @@ plt.show()
 # Save the model weights and architecture
 model.save('my_model.h5')
 
-# Create a new model that outputs the activations of each layer till layer 4
-layer_outputs = [layer.output for layer in model.layers]
+# Create a new model that outputs the activations of each layer
+layer_outputs = [
+    layer.output for layer in model.layers if isinstance(layer, Conv2D)]
 activation_model = Model(inputs=model.input, outputs=layer_outputs)
 
-# Predict the output and get the activations of each layer till layer 4
+# Predict the output and get the activations
 activations = activation_model.predict(x)
 
 # Save the output image and the activations of each layer
 for i, activation in enumerate(activations):
-    # Save the activation of each layer as a separate image till layer 4
+    # Create a directory to save the activations of each layer
+    layer_dir = os.path.join(output_dir, f'layer_{i+1}')
+    if not os.path.exists(layer_dir):
+        os.makedirs(layer_dir)
+
+    # Save the activation of each layer as a separate image
     activation = np.squeeze(activation, axis=0)
     for j in range(activation.shape[-1]):
         cv2.imwrite(
-            f'outputimgs/activation_layer_{i+1}_channel_{j+1}.jpg', activation[:, :, j])
+            os.path.join(layer_dir, f'channel_{j+1}.jpg'), activation[:, :, j])
 
 # Predict the output of the entire model
 output = model.predict(x)
